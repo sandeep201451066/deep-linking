8ccc1ca6e1f7ba6f30b55ff582340f1c5a9b69bc
Sandeep <sandeep.kumar1@rsystems.com>
2022-12-23 16:21:04 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,67 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { browserDeeplink } from "@macpaw/browser-deeplink";
+// import { browserDeeplink } from "@macpaw/browser-deeplink";
+
+function ejectIframe(iframe: any) {
+  document.body.removeChild(iframe);
+}
+
+function injectIframe(src: any) {
+  const iframe = document.createElement("iframe");
+  iframe.src = src;
+  document.body.appendChild(iframe);
+  iframe.style.width = "1px";
+  iframe.style.height = "1px";
+  iframe.style.position = "fixed";
+  iframe.style.left = "-1px";
+
+  return iframe;
+}
+
+function browserDeeplink(appLink: any, options: any = {}) {
+  const defaults = { waitTimeout: 200 };
+  options = { ...defaults, options, waitTimeout: 200 };
+
+  return new Promise((resolve, reject) => {
+    const iframe = injectIframe(appLink);
+    const timeout = setTimeout(() => {
+      window.removeEventListener("blur", windowBlurListener);
+      ejectIframe(iframe);
+      reject(Error(`Can't open ${appLink}`));
+    }, options.waitTimeout);
+
+    function windowBlurListener() {
+      window.removeEventListener("blur", windowBlurListener);
+      clearTimeout(timeout);
+      ejectIframe(iframe);
+      resolve(1);
+    }
+
+    window.addEventListener("blur", windowBlurListener);
+  });
+}
 
 function App() {
   const [type, setType] = React.useState("");
+
   React.useEffect(() => {
-    console.log("navigator.userAgent", navigator.userAgent);
-    setType(JSON.stringify(navigator.userAgent));
-    browserDeeplink("master.d3nu62uf8aj07x.amplifyapp.com://")
-      .then(() => {
-        console.log("application is requested to open");
-        setType("application is requested to open");
-        window.location.href = "master.d3nu62uf8aj07x.amplifyapp.com://";
-      })
-      .catch(() => {
-        console.log("application is not installed");
-        setType("application is not installed");
-        window.location.href = "https://play.google.com/store/apps/details?id=com.mycompany.myapp";
-      });
+    // console.log("navigator.userAgent", navigator.userAgent);
+    // setType(JSON.stringify(navigator.userAgent));
+    // browserDeeplink("master.d3nu62uf8aj07x.amplifyapp.com://")
+    //   .then(() => {
+    //     console.log("application is requested to open");
+    //     setType("application is requested to open");
+    //     window.location.href = "master.d3nu62uf8aj07x.amplifyapp.com://";
+    //   })
+    //   .catch(() => {
+    //     console.log("application is not installed");
+    //     setType("application is not installed");
+    //     window.location.href = "https://play.google.com/store/apps/details?id=com.mycompany.myapp";
+    //   });
     // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     //   try {
     //     // window.location.href = "master.d3nu62uf8aj07x.amplifyapp.com://";
-    //     // window.open("master.d3nu62uf8aj07x.amplifyapp.com://");
     //     var now = new Date().valueOf();
     //     setTimeout(function () {
     //       if (new Date().valueOf() - now > 100) return;
@@ -34,7 +73,23 @@ function App() {
     //     console.log("error--------------------", error);
     //   }
     // }
+    getLinks();
   }, []);
+
+  const getLinks = async () => {
+    try {
+      const res = await browserDeeplink("master.d3nu62uf8aj07x.amplifyapp.com://");
+      console.log("res", res);
+      console.log("application is requested to open");
+      setType("application is requested to open");
+      window.location.href = "master.d3nu62uf8aj07x.amplifyapp.com://";
+    } catch (error) {
+      console.log("error", error);
+      console.log("application is not installed");
+      setType("application is not installed");
+      window.location.href = "https://play.google.com/store/apps/details?id=com.mycompany.myapp";
+    }
+  };
 
   return (
     <div className="App">
