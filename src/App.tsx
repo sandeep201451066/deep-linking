d8c76ea0805580074cf9abc64ebb05a3ebe91ab1
Sandeep <sandeep.kumar1@rsystems.com>
2022-12-23 13:20:04 +0530

    open from web browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [type, setType] = React.useState("");
+  React.useEffect(() => {
+    console.log("navigator.userAgent", navigator.userAgent);
+    setType(JSON.stringify(navigator.userAgent));
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      try {
+        window.location.href = `master.d3nu62uf8aj07x.amplifyapp.com://`;
+      } catch (error) {
+        setType(JSON.stringify(error));
+        console.log("error--------------------", error);
+      }
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Type <code>{type}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
