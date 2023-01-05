bba13f9bd33f116eaafabfd00f8f4030b3730312
Sandeep <sandeep.kumar1@rsystems.com>
2023-01-05 16:06:31 +0530

    schemes updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-// import { browserDeeplink } from "@macpaw/browser-deeplink";
 
 function App() {
-  const [type, setType] = React.useState(0);
-  const [one, setOne] = React.useState(0);
-  const [two, settwo] = React.useState(0);
-  const [three, setThree] = React.useState(0);
-
   React.useEffect(() => {
-    // console.log("navigator.userAgent", navigator.userAgent);
-    // setType(JSON.stringify(navigator.userAgent));
-    // browserDeeplink("master.d3nu62uf8aj07x.amplifyapp.com://")
-    //   .then(() => {
-    //     console.log("application is requested to open");
-    //     setType("application is requested to open");
-    //     window.location.href = "master.d3nu62uf8aj07x.amplifyapp.com://";
-    //   })
-    //   .catch(() => {
-    //     console.log("application is not installed");
-    //     setType("application is not installed");
-    //     window.location.href = "https://play.google.com/store/apps/details?id=com.mycompany.myapp";
-    //   });
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-      var now = new Date().valueOf();
-      // setOne(now);
       try {
-        // window.location.href = "master.d3nu62uf8aj07x.amplifyapp.com://";
-        // setType(JSON.stringify(navigator.userAgent));
         setTimeout(function () {
+          // app stores link
           window.location.href = "https://play.google.com/store/apps/details?id=com.horseplay";
         }, 500);
-        window.location.href = "master.d3nu62uf8aj07x.amplifyapp.com://";
+
+        // URL schemes
+        window.location.href = "horseplay://activity/new/6800B45DE031C450C48861BBFBF7C8CE?p1=one&p2=two&p3=three";
       } catch (error) {
         console.log("error--------------------", error);
       }
@@ -42,19 +21,6 @@ function App() {
   return (
     <div>
       <header>
-        {/* <p>
-          Type <code>{type}</code>
-        </p>
-        <p>
-          One <code>{one}</code>
-        </p>
-        <p>
-          Two <code>{two}</code>
-        </p>
-        <p>
-          Three <code>{three}</code>
-        </p> */}
-
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Welcome !!!!!!
         </a>
