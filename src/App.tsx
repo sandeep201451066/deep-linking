b925217971dbeb3edd1e2b5cda62619c42d6c90e
Sandeep <sandeep.kumar1@rsystems.com>
2023-01-05 16:21:31 +0530

    schemes updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  var change = false;
   React.useEffect(() => {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
       try {
+        window.onblur = function () {
+          change = true;
+        };
+        window.onfocus = function () {
+          change = false;
+        };
         setTimeout(function () {
           // app stores link
           window.location.href = "https://play.google.com/store/apps/details?id=com.horseplay";
-        }, 500);
+        }, 1500);
 
         // URL schemes
         window.location.href = "horseplay://activity/new/6800B45DE031C450C48861BBFBF7C8CE?p1=one&p2=two&p3=three";
